8f5a82e3de90a08a448dae5e62afc639318ede2f
TonyDolenti <antoniobdolenti@gmail.com>
2022-10-21 10:09:50 -0400

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("what is your name?");
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine("what is your favorite color?");
+            //string color = Console.ReadLine();
+
+            //Console.WriteLine("what is your favorite animal?");
+            //string userAnimalName = Console.ReadLine();
+
+            //Console.WriteLine("what is your favorite artist?");
+            //string userArtist = Console.ReadLine();
+
+            //Console.WriteLine($"There was a man named {userName}, he loved his {userAnimalName}.");
+            //Console.WriteLine($"He painted his {userAnimalName} his favorite shade of {color}.");
+            //Console.WriteLine($"{userName} loved {userArtist} and listened to every song.");
+
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is : {sum}");
+
+            Console.WriteLine("Give me a number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to multiply by the first one");
+            num2 = int.Parse(Console.ReadLine());
+
+            int product = Multiply(num1, num2);
+            Console.WriteLine($"The sum is : {product}");
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
         }
     }
 }
